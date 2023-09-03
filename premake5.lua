1175b1d77075d7cc0d0e99c4a639c0cc099da90c
Liudas Augustinas <liudasaugust@gmail.com>
2023-09-03 22:31:03 +0300

    Add files via upload

---
+++ premake5.lua
@@ -0,0 +1,49 @@
+project "ImGuizmo"
+	kind "StaticLib"
+	language "C++"
+    staticruntime "on"
+
+	targetdir("%{wks.location}/Build/Binary/"..outputdir.."/%{prj.name}");
+	objdir("%{wks.location}/Build/Intermediate/"..outputdir.."/%{prj.name}");
+
+	files
+	{
+		"GraphEditor.cpp",
+		"GraphEditor.h",
+		"ImCurveEdit.cpp",
+		"ImCurveEdit.h",
+		"ImGradient.cpp",
+		"ImGradient.h",
+		"ImGuizmo.cpp",
+		"ImGuizmo.h",
+		"ImSequencer.cpp",
+		"ImSequencer.h",
+		"ImZoomSlider.h"
+	}
+	
+	includedirs
+	{
+		"%{wks.location}/Projects/ImGui"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++latest"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+    filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
